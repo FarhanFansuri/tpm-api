405bde057a678f1e1f9acd0f5c222f9b6e67a20d
admin-farhan <muhfarhan912391@gmail.com>
2023-05-15 13:10:30 +0700

    UPLOAD

--- functions/api.js
+++ functions/api.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
-const mongoose = require('mongoose');
-const serverless = require('serverless-http');
-const router = require('../routers/index')
+const fs = require('fs').promises;
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-    
-app.use('/',router)    
 
-module.exports.handler = serverless(app)
+// Get all data
+app.get('/data', (req, res) => {
+  getData()
+    .then(data => res.json(data))
+    .catch(error => {
+      console.error('Error getting data:', error);
+      res.status(500).send('Internal Server Error');
+    });
+});
 
-      
-      
+function getData() {
+  return fs.readFile('./data/hotels.json', 'utf-8')
+    .then(data => JSON.parse(data))
+    .catch(error => {
+      console.error('Error reading data file:', error);
+      throw error;
+    });
+}
 
-  
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
