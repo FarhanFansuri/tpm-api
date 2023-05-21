a1392819b2485b75cb3e46b0c5fdb9d1d9cd2230
Mochammad Farhan Fansuri <96122019+FarhanFansuri@users.noreply.github.com>
2023-05-21 16:50:15 +0700

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
-const serverless = require('serverless-http')
+const serverless = require('serverless-http');
 const app = express();
 const port = 3000;
-const data = require('../data/data.js')
-const books = require('../data/books.js')
+const data = require('../data/data.js');
+const books = require('../data/books.js');
 
 app.use(bodyParser.json());
 
-// Get all data
-app.get('/api/hotels/info', (req, res) => {
-  res.json(data)
+// Middleware untuk mengizinkan akses CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
 });
 
-//Get all data books
+// Get all data
+app.get('/api/hotels/info', (req, res) => {
+  res.json(data);
+});
+
+// Get all data books
 app.get('/api/v1/amazon/books', (req, res) => {
-  res.json(books)
+  res.json(books);
 });
 
 app.get('/', (req, res) => {
@@ -26,13 +40,13 @@ app.get('/', (req, res) => {
 app.get('/welcome', (req, res) => {
   res.send(`
   <h1 style='margin-left:50px; margin-top:40px'>Welcome to my API</h1>
-  <p style='margin-left:50px; margin-top:10px; font-size: 20px'>what's your code?<p>
-  <h5 style='margin-left:50px; margin-top:10px; font-size: 20px'>GET : /api/hotels/info</h5>
-  `)
+  <p style='margin-left:50px; margin-top:10px; font-size: 20px'>What's your code?<p>
+  <h5 style='margin-left:50px; margin-top:10px; font-size: 20px'>GET: /api/hotels/info</h5>
+  `);
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
-module.exports.handler = serverless(app)
+module.exports.handler = serverless(app);
